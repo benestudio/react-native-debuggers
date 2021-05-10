9675018e3e72772796f41b926f2357a87122526c
Fodor Dániel <daniel.fodor@benestudio.co>
2021-05-10 14:30:37 +0200

    add host to tron instance configuration

--- reactotron-config.js
+++ reactotron-config.js
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-community/async-storage';
+import { NativeModules } from 'react-native';
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 
@@ -6,8 +7,11 @@ let ReactotronInstance = null;
 
 const create = () => {
   if (__DEV__) {
+    const { scriptURL } = NativeModules.SourceCode;
+    const host = scriptURL.split('://')[1].split(':')[0];
+
     ReactotronInstance = Reactotron.setAsyncStorageHandler(AsyncStorage)
-      .configure()
+      .configure({ host })
       .useReactNative()
       .use(reactotronRedux())
       .connect();
